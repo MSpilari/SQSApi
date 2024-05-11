14a98989314c4a2279f1bb860b7ba6e83691c2f7
MSpilari <mb.spilari@hotmail.com>
2024-05-11 12:24:30 -0300

    feat: errorHandler return stack on dev environment

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -6,7 +6,14 @@ const errorHandler: ErrorRequestHandler = (
   res: Response,
   next: NextFunction
 ) => {
-  return res.status(500).json({ name: error.name, message: error.message });
+  return res
+    .status(500)
+    .json({
+      name: error.name,
+      message: error.message,
+      success: false,
+      stack: process.env.NODE_ENV === "development" ? error.stack : {},
+    });
 };
 
 export { errorHandler };
