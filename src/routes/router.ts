b2622870e74a269770c2c29b14a1f5c357d54642
MSpilari <mb.spilari@hotmail.com>
2024-05-11 12:51:44 -0300

    feat: Add validation middleware to newUser route

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -2,12 +2,14 @@ import express from "express";
 import { UserService } from "../services/UserService";
 import { userRepository } from "../repositories/UserRepository";
 import { UserController } from "../controllers/UserController";
+import { validation } from "../middlewares/validation";
+import { UserSchema } from "../schemas/UserSchema";
 
 const userService = new UserService(userRepository);
 const userController = new UserController(userService);
 
 const router = express.Router();
 
-router.post("/newUser", userController.addNewUser);
+router.post("/newUser", validation(UserSchema), userController.addNewUser);
 
 export { router };
