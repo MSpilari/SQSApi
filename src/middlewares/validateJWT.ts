8831484ca4eb1c64cd19b9fa887dfff73512999a
MSpilari <mb.spilari@hotmail.com>
2024-05-18 12:38:26 -0300

    Feat: Validate the accessToken that user sends to server

--- src/middlewares/validateJWT.ts
+++ src/middlewares/validateJWT.ts
@@ -1,5 +1,23 @@
 import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import { UserPayload } from "../DTO/UserPayload";
 
-const validateJWT = () => {
-  return (req: Request, res: Response, next: NextFunction) => {};
+const validateJWT = (secret: string) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const authToken = req.headers.authorization;
+
+    if (!authToken) throw new Error("Token not found");
+
+    const [_, token] = authToken.split(" ");
+
+    jwt.verify(token, secret, (err, user) => {
+      if (err) next(err);
+
+      req.user = user as UserPayload;
+    });
+
+    next();
+  };
 };
+
+export { validateJWT };
