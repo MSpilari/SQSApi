b3178dcb3a69388dd630e446311467ba45e1e7a9
MSpilari <mb.spilari@hotmail.com>
2024-05-18 17:35:09 -0300

    Refactor: Put PasswordCompare in his own directory

--- src/helpers/PasswordCompare.ts
+++
@@ -1,9 +0,0 @@
-import bcrypt from "bcrypt";
-
-const PasswordCompare = async (passwordInput: string, passwordDb: string) => {
-  const isEqual = await bcrypt.compare(passwordInput, passwordDb);
-
-  return isEqual;
-};
-
-export { PasswordCompare };
---
+++ src/helpers/PasswordCompare/PasswordCompare.ts
@@ -0,0 +1,19 @@
+import bcrypt from "bcrypt";
+/**
+ * Compares a plain text password with a hashed password from the database.
+ *
+ * @param passwordInput - The plain text password input.
+ * @param passwordDb - The hashed password from the database.
+ * @returns A boolean indicating whether the passwords match.
+ */
+
+const PasswordCompare = async (
+  passwordInput: string,
+  passwordDb: string
+): Promise<Boolean> => {
+  const isEqual = await bcrypt.compare(passwordInput, passwordDb);
+
+  return isEqual;
+};
+
+export { PasswordCompare };
