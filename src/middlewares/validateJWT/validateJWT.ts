8141ee68ede1b27b64d6f067f9071ca2728ead02
MSpilari <mb.spilari@hotmail.com>
2024-05-23 17:47:47 -0300

    Refactor: Insert the file inside his own directory

---
+++ src/middlewares/validateJWT/validateJWT.ts
@@ -0,0 +1,25 @@
+import type { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import type { UserPayload } from "../../DTO/UserPayload";
+
+const validateJWT = (secret: string) => {
+	return (req: Request, res: Response, next: NextFunction) => {
+		const authToken = req.headers.authorization;
+
+		if (!authToken) {
+			return next(new Error("Token not found"));
+		}
+
+		const [_, token] = authToken.split(" ");
+
+		jwt.verify(token, secret, (err, user) => {
+			if (err) next(err);
+
+			req.user = user as UserPayload;
+		});
+
+		next();
+	};
+};
+
+export { validateJWT };
