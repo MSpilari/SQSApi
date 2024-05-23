11ac5044344e9cb34740a9941b1f9ed19305b283
MSpilari <mb.spilari@hotmail.com>
2024-05-23 13:07:21 -0300

    Refactor: using type imports when needed

--- src/helpers/TryCatchHandler.ts
+++ src/helpers/TryCatchHandler.ts
@@ -1,9 +1,9 @@
-import { NextFunction, Request, Response } from "express";
+import type { NextFunction, Request, Response } from "express";
 
 const TryCatchHandler = (fn: Function) => {
-  return (req: Request, res: Response, next: NextFunction) => {
-    Promise.resolve(fn(req, res, next)).catch(next);
-  };
+	return (req: Request, res: Response, next: NextFunction) => {
+		Promise.resolve(fn(req, res, next)).catch(next);
+	};
 };
 
 export { TryCatchHandler };
