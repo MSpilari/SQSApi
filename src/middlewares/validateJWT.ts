903f3d28c83b7b1c3abfef2c8f34aee55222cd89
MSpilari <mb.spilari@hotmail.com>
2024-05-23 13:03:23 -0300

    Refactor: using type imports when needed

--- src/middlewares/validateJWT.ts
+++ src/middlewares/validateJWT.ts
@@ -1,23 +1,23 @@
-import { NextFunction, Request, Response } from "express";
+import type { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
-import { UserPayload } from "../DTO/UserPayload";
+import type { UserPayload } from "../DTO/UserPayload";
 
 const validateJWT = (secret: string) => {
-  return (req: Request, res: Response, next: NextFunction) => {
-    const authToken = req.headers.authorization;
+	return (req: Request, res: Response, next: NextFunction) => {
+		const authToken = req.headers.authorization;
 
-    if (!authToken) throw new Error("Token not found");
+		if (!authToken) throw new Error("Token not found");
 
-    const [_, token] = authToken.split(" ");
+		const [_, token] = authToken.split(" ");
 
-    jwt.verify(token, secret, (err, user) => {
-      if (err) next(err);
+		jwt.verify(token, secret, (err, user) => {
+			if (err) next(err);
 
-      req.user = user as UserPayload;
-    });
+			req.user = user as UserPayload;
+		});
 
-    next();
-  };
+		next();
+	};
 };
 
 export { validateJWT };
