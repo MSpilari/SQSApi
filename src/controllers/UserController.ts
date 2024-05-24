fb70db6ca2f9f5d1a3ecb99018718b6b9c6cdfd5
MSpilari <mb.spilari@hotmail.com>
2024-05-24 16:54:18 -0300

    Refactor: import with types

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,64 +1,64 @@
-import { Request, Response } from "express";
-import { UserService } from "../services/UserService";
+import type { Request, Response } from "express";
+import type { UserService } from "../services/UserService";
 import { TryCatchHandler } from "../helpers/TryCatchHandler";
 
 class UserController {
-  private userService;
+	private userService;
 
-  constructor(userService: UserService) {
-    this.userService = userService;
-  }
+	constructor(userService: UserService) {
+		this.userService = userService;
+	}
 
-  addNewUser = TryCatchHandler(async (req: Request, res: Response) => {
-    const newUser = await this.userService.addNewUser(req.body);
+	addNewUser = TryCatchHandler(async (req: Request, res: Response) => {
+		const newUser = await this.userService.addNewUser(req.body);
 
-    return res.status(201).json(newUser);
-  });
+		return res.status(201).json(newUser);
+	});
 
-  login = TryCatchHandler(async (req: Request, res: Response) => {
-    const { accessToken, refreshToken, user, userId } =
-      await this.userService.login(req.body);
+	login = TryCatchHandler(async (req: Request, res: Response) => {
+		const { accessToken, refreshToken, user, userId } =
+			await this.userService.login(req.body);
 
-    return res
-      .status(200)
-      .cookie("refreshToken", refreshToken, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
-        maxAge: 24 * 60 * 60 * 1000,
-      })
-      .json({ success: true, accessToken, user, userId });
-  });
+		return res
+			.status(200)
+			.cookie("refreshToken", refreshToken, {
+				httpOnly: true,
+				secure: process.env.NODE_ENV === "production",
+				sameSite: "strict",
+				maxAge: 24 * 60 * 60 * 1000,
+			})
+			.json({ success: true, accessToken, user, userId });
+	});
 
-  refreshToken = TryCatchHandler(async (req: Request, res: Response) => {
-    const { refreshToken } = req.cookies;
+	refreshToken = TryCatchHandler(async (req: Request, res: Response) => {
+		const { refreshToken } = req.cookies;
 
-    if (!refreshToken) throw new Error("Token not found");
+		if (!refreshToken) throw new Error("Token not found");
 
-    const newAccessToken = this.userService.refreshToken(refreshToken);
+		const newAccessToken = this.userService.refreshToken(refreshToken);
 
-    return res.status(200).json(newAccessToken);
-  });
+		return res.status(200).json(newAccessToken);
+	});
 
-  deleteUser = TryCatchHandler(async (req: Request, res: Response) => {
-    const userDeletedEmail = await this.userService.deleteUser(req.user!);
+	deleteUser = TryCatchHandler(async (req: Request, res: Response) => {
+		const userDeletedEmail = await this.userService.deleteUser(req.user!);
 
-    return res
-      .status(200)
-      .cookie("refreshToken", "", {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
-        expires: new Date(0),
-      })
-      .json(userDeletedEmail);
-  });
+		return res
+			.status(200)
+			.cookie("refreshToken", "", {
+				httpOnly: true,
+				secure: process.env.NODE_ENV === "production",
+				sameSite: "strict",
+				expires: new Date(0),
+			})
+			.json(userDeletedEmail);
+	});
 
-  allUsers = async (req: Request, res: Response) => {
-    const allUsers = await this.userService.listAllUsers();
+	allUsers = async (req: Request, res: Response) => {
+		const allUsers = await this.userService.listAllUsers();
 
-    return res.status(200).json(allUsers);
-  };
+		return res.status(200).json(allUsers);
+	};
 }
 
 export { UserController };
