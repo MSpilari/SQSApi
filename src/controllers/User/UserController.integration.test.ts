3d8b312e5a1eb23134e4acf0faeb5b40ede59d8e
MSpilari <mb.spilari@hotmail.com>
2024-05-27 18:06:21 -0300

    Test: if addNewUser route works properly

---
+++ src/controllers/User/UserController.integration.test.ts
@@ -0,0 +1,25 @@
+import request from "supertest";
+import { server } from "../../index";
+import { afterAll, beforeEach, describe, expect, it } from "vitest";
+import { prismaClient } from "../../db/prismaClient";
+
+describe("User Controller integration tests", () => {
+	beforeEach(async () => {
+		await prismaClient.user.deleteMany({});
+		await prismaClient.product.deleteMany({});
+		await prismaClient.category.deleteMany({});
+	});
+
+	afterAll(async () => {
+		await prismaClient.$disconnect();
+	});
+
+	it("Should add a new user", async () => {
+		const newUser = { email: "test@email.com", password: "password" };
+
+		const response = await request(server).post("/newUser").send(newUser);
+
+		expect(response.status).toBe(201);
+		expect(response.body).toHaveProperty("message");
+	});
+});
