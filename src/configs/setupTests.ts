9904eba1df52de86b83d726208fdc0a00794b068
MSpilari <mb.spilari@hotmail.com>
2024-05-27 18:03:51 -0300

    Refactor: Remove settings for all the tests

--- src/configs/setupTests.ts
+++ src/configs/setupTests.ts
@@ -1,13 +1,3 @@
 import "./dotenvConfig";
-import { afterAll, beforeEach } from "vitest";
-import { prismaClient } from "../db/prismaClient";
 
-afterAll(async () => {
-  await prismaClient.$disconnect();
-});
-
-beforeEach(async () => {
-  await prismaClient.user.deleteMany({});
-  await prismaClient.product.deleteMany({});
-  await prismaClient.category.deleteMany({});
-});
+// If you need settings for all tests, unit, integration, put them in this file
