727e3e0982bc3d48aca3c5e9f7b94518a74bef3e
MSpilari <mb.spilari@hotmail.com>
2024-05-29 13:11:58 -0300

    Refactor: AddNewCategory, updateCategory and deleteCategory methods creadted

--- src/services/CategoryService.ts
+++ src/services/CategoryService.ts
@@ -1,10 +1,23 @@
 import type { Prisma } from "@prisma/client";
 import type { DefaultArgs } from "@prisma/client/runtime/library";
+import type { Category } from "../DTO/Category";
 
 class CategoryService {
 	private categoryRepository;
 
-	constructor(categoryRepository: Prisma.UserDelegate<DefaultArgs>) {
+	constructor(categoryRepository: Prisma.CategoryDelegate<DefaultArgs>) {
 		this.categoryRepository = categoryRepository;
 	}
+
+	addNewCategory = async ({ title, description, userID }: Category) => {
+		this.categoryRepository.create({
+			data: { title, description, userID, User: { connect: { id: userID } } },
+		});
+	};
+
+	updateCategory = async () => {};
+
+	deleteCategory = async () => {};
 }
+
+export { CategoryService };
