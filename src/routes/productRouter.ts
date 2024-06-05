1d99d6effcae245d02c4332474487b33ba763a24
MSpilari <mb.spilari@hotmail.com>
2024-06-05 12:14:05 -0300

    Feat: Add the addNewProduct route

--- src/routes/productRouter.ts
+++ src/routes/productRouter.ts
@@ -4,6 +4,7 @@ import { productRepository } from "../repositories/ProductRepository";
 import { ProductController } from "../controllers/Product/ProductController";
 import { validateJWT } from "../middlewares/validateJWT/validateJWT";
 import { validation } from "../middlewares/Validation/validation";
+import { ProductSchema } from "../schemas/ProductSchema";
 
 const JWT_SECRET = process.env.JWT_SECRET || "not found, will return error";
 
@@ -13,11 +14,11 @@ const productController = new ProductController(productService);
 const router = express.Router();
 
 router.get("/allProducts", productController.allProducts);
-// router.post(
-// 	"/newProduct",
-// 	validateJWT(JWT_SECRET),
-// 	validation(CategorySchema),
-// 	productController.addNewProduct,
-// );
+router.post(
+	"/newProduct",
+	validateJWT(JWT_SECRET),
+	validation(ProductSchema),
+	productController.addNewProduct,
+);
 
 export default router;
