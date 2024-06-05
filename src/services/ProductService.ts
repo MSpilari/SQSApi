789cfd7cc82495654cb9127d9bedc3eaf5c40e9f
MSpilari <mb.spilari@hotmail.com>
2024-06-05 12:14:46 -0300

    Feat: Create the addNewProduct service

--- src/services/ProductService.ts
+++ src/services/ProductService.ts
@@ -1,10 +1,34 @@
 import type { Prisma } from "@prisma/client";
 import type { DefaultArgs } from "@prisma/client/runtime/library";
+import type { Product } from "../DTO/Product";
 
 class ProductService {
 	private productRepository;
 
-	constructor(productRepository: Prisma.UserDelegate<DefaultArgs>) {
+	constructor(productRepository: Prisma.ProductDelegate<DefaultArgs>) {
 		this.productRepository = productRepository;
 	}
+	addNewProduct = async ({
+		title,
+		description,
+		price,
+		userID,
+		categoryID,
+	}: Product) => {
+		const productExists = await this.productRepository.findFirst({
+			where: { title, categoryID, userId: userID },
+		});
+
+		if (productExists) throw new Error("This product is already created.");
+
+		return await this.productRepository.create({
+			data: { title, description, price, categoryID, userId: userID },
+		});
+	};
+
+	allProducts = async () => {
+		return await this.productRepository.findMany({});
+	};
 }
+
+export { ProductService };
