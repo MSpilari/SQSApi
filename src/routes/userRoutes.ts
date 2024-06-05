5c88b1e77022c0c8aa2a06f80261fb8e5451a08c
MSpilari <mb.spilari@hotmail.com>
2024-06-05 11:44:51 -0300

    Feat: Split the userRoutes on his own file

---
+++ src/routes/userRoutes.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import { validation } from "../middlewares/Validation/validation";
+import { UserSchema } from "../schemas/UserSchema";
+import { UserController } from "../controllers/User/UserController";
+import { validateJWT } from "../middlewares/validateJWT/validateJWT";
+import { UserService } from "../services/UserService";
+import { userRepository } from "../repositories/UserRepository";
+
+const JWT_SECRET = process.env.JWT_SECRET || "not found, will return error";
+
+const userService = new UserService(userRepository);
+const userController = new UserController(userService);
+
+const router = express.Router();
+
+router.post("/newUser", validation(UserSchema), userController.addNewUser);
+router.post("/login", validation(UserSchema), userController.login);
+router.get("/refreshToken", userController.refreshToken);
+router.get("/allUsers", userController.allUsers);
+router.delete(
+	"/deleteUser",
+	validateJWT(JWT_SECRET),
+	userController.deleteUser,
+);
+
+export default router;
