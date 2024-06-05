f99f38c33b559991a64dcffd32fd07f32bece3dd
MSpilari <mb.spilari@hotmail.com>
2024-06-05 16:30:18 -0300

    Feat: Create the updateProduct method

--- src/services/ProductService.ts
+++ src/services/ProductService.ts
@@ -1,6 +1,7 @@
 import type { Prisma } from "@prisma/client";
 import type { DefaultArgs } from "@prisma/client/runtime/library";
 import type { Product } from "../DTO/Product";
+import type { ProductUpdate } from "../DTO/ProductUpdate";
 
 class ProductService {
 	private productRepository;
@@ -8,6 +9,7 @@ class ProductService {
 	constructor(productRepository: Prisma.ProductDelegate<DefaultArgs>) {
 		this.productRepository = productRepository;
 	}
+
 	addNewProduct = async ({
 		title,
 		description,
@@ -23,6 +25,36 @@ class ProductService {
 
 		return await this.productRepository.create({
 			data: { title, description, price, categoryID, userId: userID },
+		});
+	};
+
+	updateProduct = async ({
+		productId,
+		title,
+		description,
+		price,
+		userID,
+	}: ProductUpdate) => {
+		await this.productRepository.findFirstOrThrow({
+			where: { userId: userID, id: productId },
+		});
+
+		const productExists = await this.productRepository.findFirst({
+			where: { userId: userID, title },
+		});
+		if (productExists)
+			throw new Error(
+				"Can't update a product with a title that already exists.",
+			);
+
+		const updatedData: any = {};
+		if (title) updatedData.title = title;
+		if (description) updatedData.description = description;
+		if (price) updatedData.price = price;
+
+		return await this.productRepository.update({
+			where: { userId: userID, id: productId },
+			data: updatedData,
 		});
 	};
 
