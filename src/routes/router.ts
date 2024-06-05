ed0c2eada7a98e2803cd5cfe29df370c63a1ffcb
MSpilari <mb.spilari@hotmail.com>
2024-06-05 11:48:26 -0300

    Refactor: Add the separated route files to the router

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,55 +1,17 @@
 import express from "express";
-import { UserService } from "../services/UserService";
-import { userRepository } from "../repositories/UserRepository";
-import { UserController } from "../controllers/User/UserController";
-import { validation } from "../middlewares/Validation/validation";
-import { UserSchema } from "../schemas/UserSchema";
-import { validateJWT } from "../middlewares/validateJWT/validateJWT";
-import { CategoryService } from "../services/CategoryService";
-import { categoryRepository } from "../repositories/CategoryRepository";
-import { CategoryController } from "../controllers/Category/CategoryController";
-import { CategorySchema } from "../schemas/CategorySchema";
-import { CategoryUpdateSchema } from "../schemas/CategoryUpdateSchema";
-
-const JWT_SECRET = process.env.JWT_SECRET || "not found, will return error";
-
-const userService = new UserService(userRepository);
-const userController = new UserController(userService);
-
-const categoryService = new CategoryService(categoryRepository);
-const categoryController = new CategoryController(categoryService);
+import categoryRoutes from "./categoryRoutes";
+import userRoutes from "./userRoutes";
+import productRoutes from "./productRouter";
 
 const router = express.Router();
 
-// User Routes
-router.post("/newUser", validation(UserSchema), userController.addNewUser);
-router.post("/login", validation(UserSchema), userController.login);
-router.get("/refreshToken", userController.refreshToken);
-router.get("/allUsers", userController.allUsers);
-router.delete(
-	"/deleteUser",
-	validateJWT(JWT_SECRET),
-	userController.deleteUser,
-);
+// User routes
+router.use(userRoutes);
 
-// Category Routes
-router.get("/allCategories", categoryController.allCategories);
-router.post(
-	"/newCategory",
-	validateJWT(JWT_SECRET),
-	validation(CategorySchema),
-	categoryController.addNewCategory,
-);
-router.put(
-	"/updateCategory/:id",
-	validateJWT(JWT_SECRET),
-	validation(CategoryUpdateSchema),
-	categoryController.updateCategory,
-);
-router.delete(
-	"/deleteCategory/:id",
-	validateJWT(JWT_SECRET),
-	categoryController.deleteCategory,
-);
+// Category routes
+router.use(categoryRoutes);
+
+// Product routes
+router.use(productRoutes);
 
 export { router };
