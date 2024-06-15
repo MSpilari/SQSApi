91238504dfecd274edeebe1401d6d55d118ba20f
MSpilari <mb.spilari@hotmail.com>
2024-06-15 18:14:44 -0300

    Refactor: consumer can listen to delete and add objects

--- src/rabbitmq/consumer.ts
+++ src/rabbitmq/consumer.ts
@@ -1,4 +1,8 @@
 import amqp from "amqplib/callback_api";
+import { prismaClient } from "../db/prismaClient";
+import { minioClient } from "../configs/minIOClient";
+import { saveUpdateObject } from "../minioMethods/saveUpdateObject";
+import { deleteObject } from "../minioMethods/deleteObject";
 
 const rabbitmqUrl = process.env.RABBITMQ_URL;
 
@@ -19,12 +23,59 @@ const consumer = (exchange: string, queue: string) => {
 				` [*] Waiting for messages in ${queue}. To exit press CTRL+C`,
 			);
 
-			channel.consume(queue, async (msg) => {
-				if (msg !== null) {
-					console.log(msg);
-					channel.ack(msg);
-				}
-			});
+			channel.consume(
+				queue,
+				async (msg: any) => {
+					if (msg !== null) {
+						const message = JSON.parse(msg.content.toString());
+						const { userId, type } = message;
+
+						const catalog = await prismaClient.user.findMany({
+							where: { id: userId },
+							select: {
+								id: true,
+								email: true,
+								categories: {
+									select: {
+										id: true,
+										title: true,
+										description: true,
+										products: {
+											select: {
+												id: true,
+												title: true,
+												description: true,
+												price: true,
+												categoryID: true,
+												userId: true,
+											},
+										},
+									},
+								},
+							},
+						});
+
+						const catalogJSON = JSON.stringify(catalog);
+
+						// Armazenar JSON no MinIO
+						const bucketName = "catalogos";
+						const objectName = `${userId}-catalogo.json`;
+
+						const bucketExists = await minioClient.bucketExists(bucketName);
+
+						if (!bucketExists) await minioClient.makeBucket(bucketName, "");
+
+						if (type === "ADD_OBJECT")
+							await saveUpdateObject(bucketName, objectName, catalogJSON);
+
+						if (type === "DELETE_OBJECT")
+							await deleteObject(bucketName, objectName);
+
+						channel.ack(msg);
+					}
+				},
+				{ noAck: false },
+			);
 		});
 	});
 };
